a7c3986290c3de573ac89d68633bc8a61c5cf3e2
soowan <tndhks0708@naver.com>
2024-12-03 09:34:19 +0900

    헤더 고정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
-import PlayerList from './PlayerList'
-import Header from './Header'
+import PlayerList from './PlayerList';
+import Header from './Header';
 import Footer from './Footer';
 
 function App() {
   return (
     <div>
+      {/* 고정된 헤더 */}
       <Header />
-      <PlayerList/>
-      <Footer/>
+
+      {/* 본문 컨텐츠를 헤더 높이만큼 아래로 밀기 */}
+      <div className="body-padding">
+        <PlayerList />
+      </div>
+
+      {/* 푸터 */}
+      <Footer />
     </div>
   );
 }
